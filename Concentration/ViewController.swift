8a57f89f5abec615eaa308f73130f54d9cb2eab9
Dmitriy Kheroim <trackensure.test@icloud.com>
2021-08-16 22:05:02 +0300

    Create project

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Track Ensure on 2021-08-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
